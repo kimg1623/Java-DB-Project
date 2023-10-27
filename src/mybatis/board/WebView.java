a31550cfafa53d2b3e278f967e2e32a153e640ec
Jiwon <kimg1623@naver.com>
2023-10-27 15:57:54 +0900

    :lipstick: Update: 게시물 전체 목록 출력 UI 변경

--- src/mybatis/board/WebView.java
+++ src/mybatis/board/WebView.java
@@ -2,20 +2,26 @@ package mybatis.board;
 
 import mybatis.dbtest.Addr;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class WebView {
 
 	/* 게시물 전체 출력 */
 	public void printBoards(ArrayList<Board> BoardList) {
-		System.out.println("========= [게시물 전체 읽기] =========");
+		System.out.println("---------------------------[게시물 전체 목록]---------------------------");
+		System.out.printf("%-6s%-10s%-16s%-40s\n", "no", "writer", "date", "title");
+		System.out.println("-----------------------------------------------------------------------");
+
+		DateFormat dateFomatter = new SimpleDateFormat("yyyy-MM-dd");
 		for(int i = 0; i < BoardList.size(); i++) {
-			System.out.println("번호 : " + BoardList.get(i).getBno());
-			System.out.println("제목 : " + BoardList.get(i).getBtitle());
-			System.out.println("내용 : " + BoardList.get(i).getBcontent());
-			System.out.println("작성자 : " + BoardList.get(i).getBwriter());
-			System.out.println("날짜: " + BoardList.get(i).getBdate());
-			System.out.println("=============================");
+			System.out.printf("%-6s%-10s%-16s%-40s \n",
+					BoardList.get(i).getBno(),
+					BoardList.get(i).getBwriter(),
+					dateFomatter.format(BoardList.get(i).getBdate()),
+					BoardList.get(i).getBtitle());
 		}
 	}
 
--- src/mysql/board/Board.java
+++ src/mysql/board/Board.java
@@ -2,8 +2,12 @@ package mysql.board;
 
 import java.util.Date;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class Board {
     private int bno;
     private String btitle;
